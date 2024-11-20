b64c991c67a8b40a7255e6645e0e3fad5c7af967
DearlyMikey <mvgonza@clemson.edu>
2024-11-20 15:59:45 -0500

    Finished ProblemSet08

--- PEMod8/ProblemSet08/ProblemSet08-Problem04.sql
+++ PEMod8/ProblemSet08/ProblemSet08-Problem04.sql
@@ -5,4 +5,5 @@
 -- 
 
 /* YOUR SOLUTION HERE */
+COMMIT;
 
--- PEMod8/ProblemSet08/ProblemSet08-Problem06.sql
+++ PEMod8/ProblemSet08/ProblemSet08-Problem06.sql
@@ -7,4 +7,8 @@
 --
 
 /* YOUR SOLUTION HERE */
-
+DELETE FROM EMP_1
+WHERE EMP_LNAME = 'Smithfield'
+    AND EMP_FNAME = 'William'
+    AND EMP_HIREDATE = '2004-06-22'
+    AND JOB_CODE = '500';
